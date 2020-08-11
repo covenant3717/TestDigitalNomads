79170a73d1a3f0a08eba72253d2b50b28a5b3392
Evgeny Kuksov <Covenant3717@gmail.com>
2020-08-11 11:57:34 +0700

    refactoring

--- app/src/main/java/com/evgeny/testdigitalnomads/repository/Repository.kt
+++ app/src/main/java/com/evgeny/testdigitalnomads/repository/Repository.kt
@@ -8,9 +8,7 @@ import com.evgeny.testdigitalnomads.repository.network.MainApi
 import com.evgeny.testdigitalnomads.repository.network.MainApiClient
 import com.evgeny.testdigitalnomads.repository.network.NewsPagingSource
 import com.evgeny.testdigitalnomads.repository.room.MainDao
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
 
 
 class Repository constructor(
@@ -19,17 +17,7 @@ class Repository constructor(
     private val mainApi: MainApi
 ) {
 
-    private fun loading(load: Boolean) = Resource.Progress(load)
-
     //==============================================================================================
-
-    private suspend fun saveNews(listNetNews: List<DBNews>) = withContext(Dispatchers.IO) {
-        mainDao.insert(listNetNews)
-    }
-
-    suspend fun clearCache() = withContext(Dispatchers.IO) {
-        mainDao.clearNews()
-    }
 
     suspend fun getNewsFlow(): Flow<PagingData<DBNews>> {
         return Pager(PagingConfig(pageSize = 5, prefetchDistance = 5)) {
